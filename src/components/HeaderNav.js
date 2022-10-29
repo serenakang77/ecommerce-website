29d97681a4a16c2b70c454da9249572b01da407e
serenakang <serenakang77@gmail.com>
2022-10-29 14:38:58 -0400

    Changed some styles

--- src/components/HeaderNav.js
+++ src/components/HeaderNav.js
@@ -1,16 +1,21 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons"
+
 const HeaderNav = () => {
-    return (
-      <nav className='top-nav-container'>
-        <ul className='header-wrapper'>
-          <li>
-            <a href=''>LOGO</a>
-          </li>
-          <li>
-            <a href=''>Cart</a>
-          </li>
-        </ul>
-      </nav>
-    )
+  return (
+    <nav className='top-nav-container'>
+      <ul className='header-wrapper'>
+        <li>
+          <a href='/'>LOGO</a>
+        </li>
+        <li>
+          <a href=''>
+            <FontAwesomeIcon icon={faCartShopping} />
+          </a>
+        </li>
+      </ul>
+    </nav>
+  )
 }
 
 export default HeaderNav
--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -1,7 +1,7 @@
-import { Link, useNavigate } from "react-router-dom"
+import { Link, useNavigate, useParams } from "react-router-dom"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faLeftLong } from "@fortawesome/free-solid-svg-icons"
-
+import { useState, useEffect } from "react"
 const ProductDetails = ({
   product,
   setProduct,
@@ -12,29 +12,50 @@ const ProductDetails = ({
   productId,
   setProductId,
 }) => {
-  const individualObject = filteredArray.filter(({ id }) => id == productId)
-
+    const [singleItem, setSingleItem] = useState([])
     const navigate = useNavigate();
+    const { id } = useParams();
 
+    useEffect(() => {
+      async function getApiData() {
+        const response1 = await fetch(
+          `http://makeup-api.herokuapp.com/api/v1/products.json`
+        )
+        const data1 = await response1.json()
+        const filteredProduct1 = data1.filter((individual) => individual.id == id)
+        setSingleItem(filteredProduct1)
+      }
+      getApiData()
+    }, [id])
+    
   return (
-    <div className='productWrapper productDetail'>
-      <div className='productContainer'>
-          <FontAwesomeIcon
-            icon={faLeftLong}
-            onClick={() => {
-              navigate(-1)
-            }}
-          />
-        <h2>{individualObject[0].brand}</h2>
-        <h2>{individualObject[0].name}</h2>
-        <p>${individualObject[0].price}</p>
-        <p>{individualObject[0].description}</p>
-        <button>Add To Cart</button>
-      </div>
-      <div className='productImageContainer'>
-        <img src={individualObject[0].api_featured_image} alt={individualObject[0].name} />
-      </div>
-    </div>
+    <>
+      {singleItem.length === 0 ? (
+        "Null"
+      ) : (
+        <div className='productWrapper productDetail'>
+          <div className='productContainer'>
+            <FontAwesomeIcon
+              icon={faLeftLong}
+              onClick={() => {
+                navigate(-1)
+              }}
+            />
+            <h2>{singleItem[0].brand}</h2>
+            <h2>{singleItem[0].name}</h2>
+            <p>${singleItem[0].price}</p>
+            <p>{singleItem[0].description}</p>
+            <button>Add To Cart</button>
+          </div>
+          <div className='productImageContainer'>
+            <img
+              src={singleItem[0].api_featured_image}
+              alt={singleItem[0].name}
+            />
+          </div>
+        </div>
+      )}
+    </>
   )
 }
 
